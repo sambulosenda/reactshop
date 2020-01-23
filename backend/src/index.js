0643ff1e7ed52ad0a43956eeb52d17160960c57a
sambulo senda <sambulosendas@gmail.com>
2020-01-23 13:42:18 +0000

    Open greaph yoga

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,17 @@
 // let's go!
+require('dotenv').config({path: 'variables.env'});
+const createServer = require('./createServer');
+const bd = require('./db');
+
+const server = createServer();
+
+///
+server.start({
+    cors: {
+        credentials: true,
+        origin: Process.env.FRONTEND_URL,
+    },
+}, deets => {
+    console.log(`Server now running on port http://localhost:${deets.port}`)
+
+});

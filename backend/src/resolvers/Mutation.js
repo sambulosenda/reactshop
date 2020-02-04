e8675c525a3b0404410ab49d2ceed6a9900777b8
sambulo senda <sambulosendas@gmail.com>
2020-02-04 09:01:00 +0000

    querys now working

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,9 +1,23 @@
 const Mutations = {
-    createDog(parent, args, ctx, info) {
-        //create a do
-
-        console.log(args);
-    }
-};
-
-module.exports = Mutations;
+    async createItem(parent, args, ctx, info) {
+      // TODO: Check if they are logged in
+      const item = await ctx.db.mutation.createItem(
+        {
+          data: {
+            ...args,
+          },
+        },
+        info
+      );
+      console.log(item);
+      return item;
+    },
+    // createDog(parent, args, ctx, info) {
+    //   global.dogs = global.dogs || [];
+    //   // create a dog
+    //   const newDog = { name: args.name };
+    //   global.dogs.push(newDog);
+    //   return newDog;
+    // },
+  };
+  module.exports = Mutations;
